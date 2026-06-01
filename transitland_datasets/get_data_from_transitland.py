daf98d9faa06401d2918d502d0343ddb35985588
sashera1 <ukk8zb@virginia.edu>
2026-06-01 18:48:59 -0400

    up until stop_ids by route

--- transitland_datasets/get_data_from_transitland.py
+++ transitland_datasets/get_data_from_transitland.py
@@ -1,5 +1,9 @@
 import csv
 
+"""
+the point of this script is to get all stops by each route
+and their coordinates"""
+
 """Corridors
 
 Orange Corridor
@@ -27,9 +31,29 @@ for corridor, routes in Corridors.items():
         trips_by_route[str(route)] = []
 
 
-with open("transitland_datasets/transitland_wrta_latest/routes.txt",'r') as routes:
-    reader = csv.DictReader(routes)
+with open("transitland_datasets/transitland_wrta_latest/trips.txt",'r') as trips:
+    reader = csv.DictReader(trips)
     for row in reader:
         if row['route_id'] in trips_by_route.keys():
-            trips_by_route[row['route_id']].append(row)
-            print(row)
+            trips_by_route[row['route_id']].append(row['trip_id'])
+
+stop_ids_by_route = {}
+for corridor, routes in Corridors.items():
+    for route in routes:
+        stop_ids_by_route[str(route)] = set()
+
+# takes a few sec to run
+# exponential time but for practical purposes,
+#linear when bounded by set #of trips
+with open("transitland_datasets/transitland_wrta_latest/stop_times.txt",'r') as stop_times:
+    reader = csv.DictReader(stop_times)
+    for row in reader:
+        for route, trips in trips_by_route.items():
+            if row['trip_id'] in trips:
+                stop_ids_by_route[route].add(row['stop_id'])
+
+# for route, stop_ids in stop_ids_by_route.items():
+#     print(f"Route {route} has {len(stop_ids)} stops")
+
+
+
