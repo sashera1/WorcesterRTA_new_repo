a287e6951e6471046ba1ab4c1bd7f05b9054fea5
sashera1 <ukk8zb@virginia.edu>
2026-06-01 18:14:14 -0400

    added data and start of code

---
+++ rapid_transit_lines_legend.py
@@ -0,0 +1,21 @@
+Corridors = {
+    'Orange':[19,27,33], 
+    'Blue':[5,12], #maybe include 12E?
+    'Green':[23,26]
+}
+"""Corridors
+
+Orange Corridor
+Routes: 19, 27, 33
+From: Central Hub → Curtis St
+Shared stops: 16
+
+Blue Corridor
+Routes: 5, 12
+From: Central Hub → Sunderland
+Shared stops: 23
+
+Green Corridor
+Routes: 23, 26
+From: Catherine → Lincoln Plaza
+Shared stops: 13"""
---
+++ transitland_datasets/get_data_from_transitland.py
@@ -0,0 +1,4 @@
+import json
+
+with open("transitland_datasets/transitland_wrta_latest/routes.txt",'r') as routes:
+    print(routes.read())
